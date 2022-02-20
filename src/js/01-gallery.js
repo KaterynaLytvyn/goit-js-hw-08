dbb0b62b26c2314de986c532ddbf0f9e0a3d3929
Kateryna Lytvyn <sam.gilmore@gmail.com>
2022-02-20 16:35:31 +0200

    added task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,17 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+//const simpleLightbox = require('simplelightbox')
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const galleryElement = document.querySelector('.gallery');
+const markup = galleryItems.map((galleryItem) => `<a class="gallery__link" href="${galleryItem.original}">
+<img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"></a>`).join("");
+
+galleryElement.insertAdjacentHTML(`beforeend`,markup);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
