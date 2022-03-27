6c40bc215a1f6d356cb9f4172feff207ef9fc018
Kateryna Lytvyn <sam.gilmore@gmail.com>
2022-03-27 15:16:22 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+var throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const email = form.querySelector('[name="email"]');
+const message = form.querySelector('[name="message"]');
+
+
+updateOutput();
+form.addEventListener("input", throttle(onFormInput, 500));
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormInput(evt) {
+
+     const feedbackFormState = {
+        email: email.value,
+        message: message.value,
+    };
+
+   localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
+}
+
+function onFormSubmit(evt) {
+
+    console.log('localStorage:', JSON.parse(localStorage.getItem("feedback-form-state")));
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.clear();
+}
+
+function updateOutput() {
+    const data = localStorage.getItem("feedback-form-state");
+
+    if (data){
+        parsedData = JSON.parse(data);
+        form.elements.email.value = parsedData.email;
+        form.elements.message.value = parsedData.message;
+    }
+}
